5798fdf27ddbd69874a172b035508f178b3da5fc
LucasAH9 <lucasandosilla@gmail.com>
2023-11-11 21:12:11 +0100

    Implementacion Cuadro Informativo
    - Se han añadido eventos al trigger del prefab Tile
    - Añadido cartel informativo en mapa al tener el mouse encima de una casilla

--- Assets/Scripts/Mapas/Tile.cs
+++ Assets/Scripts/Mapas/Tile.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
+using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
@@ -32,10 +33,13 @@ public class Tile : MonoBehaviour
     public bool selected = false;
     public List<Tile> AdyacentList = new List<Tile>();
 
+    GameObject casillaInfo;
+    private TextMeshProUGUI textoInfo;
+
+
     public void ColorTile(Color color)
     {
         _spriteRenderer.color = color;
-        Debug.Log("Quien te cambio diablo"+color.ToString());
     }
 
     //Asigna el tipo de casilla aleatoriamente en funcion de random 
@@ -45,14 +49,17 @@ public class Tile : MonoBehaviour
         int random = Random.Range(0, 100);
         _clickEvent.enabled = false;
 
+        casillaInfo = GameObject.Find("CasillaInfo");       // Asegurar que coincide con el nombre en el editor
+        textoInfo = casillaInfo.GetComponentInChildren<TextMeshProUGUI>();
+
         this.position = position;
 
         if (random >= 50) PuzzleTile();
-        else if(random <= 5) BonfireTile();
+        else if (random <= 5) BonfireTile();
         else ObstacleTile();
     }
 
-   public void LoadTile()
+    public void LoadTile()
     {
         _clickEvent.enabled = false;
 
@@ -66,17 +73,17 @@ public class Tile : MonoBehaviour
     //Carga las siguientes casillas disponibles en funcion de la lista de adyacencias 
     public void LoadNextTiles()
     {
-        if(AdyacentList.Count <= 0) { ScenesManager.instance.LoadScene(ScenesManager.Scene.EndScene); }
+        if (AdyacentList.Count <= 0) { ScenesManager.instance.LoadScene(ScenesManager.Scene.EndScene); }
 
         for (int i = 0; i <= AdyacentList.Count - 1; i++)
         {
             AdyacentList[i]._clickEvent.enabled = true;
             //AdyacentList[i]._spriteRenderer.color = Color.blue;
-         
+
             //Si es puzzle activa su animacion
             if (AdyacentList[i].type == 0) { AdyacentList[i].animatorTile.runtimeAnimatorController = animationPuzzleImage; }
             else { AdyacentList[i]._spriteRenderer.color = Color.blue; }
-           
+
         }
     }
 
@@ -87,14 +94,14 @@ public class Tile : MonoBehaviour
         var puzzleList = ScenesManager.instance.escenasPuzle;
         index = Random.Range(0, puzzleList.Count);
 
-       
+
         animatorTile = GetComponent<Animator>();
         _spriteRenderer.sprite = _puzzleImage.sprite;
 
-        
+
     }
 
-    private void ObstacleTile() 
+    private void ObstacleTile()
     {
         _spriteRenderer.sprite = _obstacleImage.sprite;
         energyCost = Random.Range(30, 40);
@@ -102,12 +109,42 @@ public class Tile : MonoBehaviour
         type = 1;
     }
 
-    private void BonfireTile() 
+    private void BonfireTile()
     {
         _spriteRenderer.sprite = _bonfireImage.sprite;
         energyCost = Random.Range(40, 60);
 
         type = 2;
     }
-    
+
+    private void ShowInfo()
+    {
+
+        casillaInfo.SetActive(true);
+        casillaInfo.transform.position = gameObject.transform.position + new Vector3(1.2f, 0.70f, 0f);
+
+        if (_clickEvent.enabled == true)
+        {
+
+            if (type == 0)
+            {
+                textoInfo.text = "Tipo: Puzzle\n Coste: " + this.energyCost;
+            }
+            if (type == 1)
+            {
+                textoInfo.text = "Tipo: Evento\n Coste: " + this.energyCost;
+            }
+            if (type == 2)
+            {
+                textoInfo.text = "Tipo: Hogera\n Coste: " + this.energyCost;
+            }
+        }
+
+    }
+
+    public void HideInfo()
+    {
+        casillaInfo.SetActive(false);
+    }
+
 }

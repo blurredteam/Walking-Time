0e371bf0e8ae442534fe7bdf499acae7797b117a
JoseMilan19 <jose.atm.wv@gmail.com>
2023-11-17 11:30:32 +0100

    Tutorial
    tutorial y correcrtrion en timer

--- Assets/Scripts/Tutorial/TutorialFunc.cs
+++ Assets/Scripts/Tutorial/TutorialFunc.cs
@@ -16,11 +16,35 @@ public class TutorialFunc : MonoBehaviour
     {
         saltarTuto.onClick.AddListener(delegate { SaltarTutorial(); });
 
-        _btnSiguiente[0].onClick.AddListener(delegate { _paneles[0].SetActive(false); _paneles[1].SetActive(true); });
-        _btnSiguiente[1].onClick.AddListener(delegate { _paneles[1].SetActive(false); _paneles[2].SetActive(true); });
+        for (int i = 0; i < 8; i++)
+        {
+            int currentIndex = i;
+            _btnSiguiente[i].onClick.AddListener(delegate {
+                _paneles[currentIndex].SetActive(false);
+                _paneles[currentIndex + 1].SetActive(true);
+            });
+        }
+        //_btnSiguiente[0].onClick.AddListener(delegate { _paneles[0].SetActive(false); _paneles[1].SetActive(true); });
+        //_btnSiguiente[1].onClick.AddListener(delegate { _paneles[1].SetActive(false); _paneles[2].SetActive(true); });
+        //_btnSiguiente[2].onClick.AddListener(delegate { _paneles[1].SetActive(false); _paneles[2].SetActive(true); });
+        //_btnSiguiente[3].onClick.AddListener(delegate { _paneles[1].SetActive(false); _paneles[2].SetActive(true); });
+        //_btnSiguiente[4].onClick.AddListener(delegate { _paneles[1].SetActive(false); _paneles[2].SetActive(true); });
+        //_btnSiguiente[5].onClick.AddListener(delegate { _paneles[1].SetActive(false); _paneles[2].SetActive(true); });
+        //_btnSiguiente[6].onClick.AddListener(delegate { _paneles[1].SetActive(false); _paneles[2].SetActive(true); });
+        //_btnSiguiente[7].onClick.AddListener(delegate { _paneles[1].SetActive(false); _paneles[2].SetActive(true); });
+        //_btnSiguiente[8].onClick.AddListener(delegate { _paneles[1].SetActive(false); _paneles[2].SetActive(true); });
         //_btnSiguiente[2].onClick.AddListener(delegate { _paneles[0].SetActive(false); _paneles[1].SetActive(true); });
-        _btnAnterior[1].onClick.AddListener(delegate { _paneles[0].SetActive(true); _paneles[1].SetActive(false); });
-        _btnAnterior[2].onClick.AddListener(delegate { _paneles[1].SetActive(true); _paneles[2].SetActive(false); });
+        //_btnAnterior[1].onClick.AddListener(delegate { _paneles[0].SetActive(true); _paneles[1].SetActive(false); });
+        //_btnAnterior[2].onClick.AddListener(delegate { _paneles[1].SetActive(true); _paneles[2].SetActive(false); });
+
+        for (int i = 1; i <= 8; i++)
+        {
+            int currentIndex = i;
+            _btnAnterior[i].onClick.AddListener(delegate {
+                _paneles[currentIndex - 1].SetActive(true);
+                _paneles[currentIndex].SetActive(false);
+            });
+        }
 
     }
 

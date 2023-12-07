084d1ab776713ac3d8349706966ba445efddbdea
LucasAH9 <lucasandosilla@gmail.com>
2023-12-07 20:54:32 +0100

    Puzle laberinto funcional + ajustes
    (de momento tiene el icono del del geometry dash)

---
+++ Assets/Scripts/Puzzles/PuzzleLaberinto/SalidaLaberinto.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SalidaLaberinto : MonoBehaviour
+{
+    public GameObject panel;
+    public GameObject botonSalir;
+
+    private Transitioner transition;
+    public float transitionTime = 1f;
+    [SerializeField] private Button continueBtn;
+
+    void Start()
+    {
+        transition = ScenesManager.instance.transitioner;
+        panel.SetActive(false);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            AudioManager.instance.WinMusic();
+            panel.SetActive(true);
+            botonSalir.SetActive(false);
+        }
+    }
+
+    public void SalirDelJuegoVictoria()
+    {
+        AudioManager.instance.ButtonSound();
+
+        LevelManager.instance.gold += 10;
+
+        StartCoroutine(FadeLaberinto());
+    }
+    public void SalirDelJuegoDerrota()
+    {
+        AudioManager.instance.ButtonSound();
+
+        AudioManager.instance.LoseMusic();
+        LevelManager.instance.teamEnergy -= 10 * LevelManager.instance.expEnergy;
+        LevelManager.instance.expEnergy += 1;
+
+        StartCoroutine(FadeLaberinto());
+    }
+
+    IEnumerator FadeLaberinto()
+    {
+        continueBtn.interactable = false;
+        transition.DoTransitionOnce();
+
+        yield return new WaitForSeconds(1f);
+
+        continueBtn.interactable = true;
+        transition.DoTransitionOnce();
+
+        ScenesManager.instance.UnloadTile(ScenesManager.Scene.PuzzleLaberinto);
+        LevelManager.instance.ActivateScene();
+        AudioManager.instance.PlayAmbient();
+    }
+}

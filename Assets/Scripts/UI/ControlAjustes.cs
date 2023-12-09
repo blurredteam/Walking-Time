6d4f89d2c1b5bb0a0ea1992865b053fbd47ead67
JoseMilan19 <jose.atm.wv@gmail.com>
2023-12-09 20:34:37 +0100

    Menú de ajustes en todo el juego

--- Assets/Scripts/UI/ControlAjustes.cs
+++ Assets/Scripts/UI/ControlAjustes.cs
@@ -1,25 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
-using TMPro;
 
-public class ControlPantallaCoompleta : MonoBehaviour
+public class ControlAjustes : MonoBehaviour
 {
     [SerializeField] private Toggle toggle;
     [SerializeField] TMP_Dropdown opcionesResolucion;
     Resolution[] resoluciones;
-    
+   
 
-    private void Awake()
-    {
-        
-        DontDestroyOnLoad(gameObject);
-    }
+    //Brillo
+    [SerializeField] private Slider slider;
+    [SerializeField] private float valorSlider;
+    [SerializeField] private Image panelBrillo;
+
+    //private void Awake()
+    //{
+       
+    //    DontDestroyOnLoad(gameObject);
+    //}
     void Start()
     {
-        
-        if(Screen.fullScreen)
+        //DontDestroyOnLoad(gameObject);
+        slider.value = PlayerPrefs.GetFloat("brillo", 0.5f);
+        panelBrillo.color = new Color(panelBrillo.color.r, panelBrillo.color.g, panelBrillo.color.b, 1 - slider.value);
+
+        if (Screen.fullScreen)
         {
             toggle.isOn = true;
         }
@@ -41,13 +49,13 @@ public class ControlPantallaCoompleta : MonoBehaviour
         opcionesResolucion.ClearOptions();
         List<string> opc = new List<string>();
         int resolucionActual = 0;
-        for(int i = 0; i<resoluciones.Length; i++)
+        for (int i = 0; i < resoluciones.Length; i++)
         {
             string opcion = resoluciones[i].width + " x " + resoluciones[i].height;
             opc.Add(opcion);
 
-            if(Screen.fullScreen && resoluciones[i].width == Screen.currentResolution.width && resoluciones[i].height == Screen.currentResolution.height)
-                { resolucionActual = i; }
+            if (Screen.fullScreen && resoluciones[i].width == Screen.currentResolution.width && resoluciones[i].height == Screen.currentResolution.height)
+            { resolucionActual = i; }
         }
 
         opcionesResolucion.AddOptions(opc);
@@ -62,5 +70,10 @@ public class ControlPantallaCoompleta : MonoBehaviour
         Resolution resolucion = resoluciones[id];
         Screen.SetResolution(resolucion.width, resolucion.height, Screen.fullScreen);
     }
-
+    public void CambiarBrillo(float valor)
+    {
+        valorSlider = valor;
+        PlayerPrefs.SetFloat("brillo", slider.value);
+        panelBrillo.color = new Color(panelBrillo.color.r, panelBrillo.color.g, panelBrillo.color.b, 1 - slider.value);
+    }
 }
